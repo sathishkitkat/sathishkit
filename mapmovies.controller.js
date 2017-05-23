7ec76d50cfab86096574e494b64b4f9e7d91e6ac
sathishkitkat <sathishkitkat001@gmail.com>
2017-05-23 20:19:26 +0530

    Add files via upload

--- mapmovies.controller.js
+++ mapmovies.controller.js
@@ -10,24 +10,48 @@ class MapmoviesComponent {
       this.data = [];
       this.socket = socket;
       this.mapmovieid='';
+      this.len='';
+      this.i='';
+      this.arr=[];
       this.tdata=[];
+      this.st='';
+      // var data1='';
       $scope.$on('$destory',function() {
         socket.unsyncupdates('mapmovieendpoints');
       });
   }
+
   Addmapmovie() {
+    this.len=$('#mySelect').children('option').length;
+     for (this.i = 0; this.i < this.len; this.i++) {
+      this.arr[this.i]=$('#mySelect option').eq(this.i).val();
+    }
     this.$http.post('/api/mapmovieendpoints',{
      location:this.location,
       theatrename:this.theatrename,
       moviesname:this.moviesname,
-      showtime:this.showtime,
-      fromdate:this.fromdate,
-      todate:this.todate
+      showtime:this.showtime
+      // fromdate:this.fromdate,
+      // todate:this.todate
     }).then (response => {
-        console.log(response)
-      this.mapmoviesDate = response.data;
+        console.log(response);
+        //  console.log("mapmovies added");
+      // this.mapmoviesDate = response.data;
       });
- }
+    }
+    getshowtime(){
+      var data1=  this.tdata;
+    var uniqueNames = [];
+                                                                var uniqueObj = [];
+                                                                for(var i = 0; i< data1.length; i++){
+                                                                    if(data1[i].theatrename==this.tname){
+                                                                    if(uniqueNames.indexOf(data1[i].showtime) === -1){
+                                                                        uniqueObj.push(data1[i])
+                                                                        uniqueNames.push(data1[i].showtime);}}
+                                                                        console.log(uniqueNames);
+                                                                        this.st=uniqueNames;
+    }
+  }
  $onInit() {
     this.$http.get('/api/moviesendpoints').then(response => {
     this.data = response.data;
@@ -35,9 +59,35 @@ class MapmoviesComponent {
    });
    this.$http.get('/api/theatreendpoints').then(response=> {
   this.tdata = response.data;
+  console.log(response.data);
+  // data1=  this.tdata;
+
+
+
 });
 
 }
+Add() {
+this.data = document.getElementById('UserSelector').value;
+// alert(this.data);
+$('#mySelect').append("<option value='"+this.data+"'>"+this.data+"</option>");
+}
+
+// this.data.ready( function() {
+//         my_basic_cal = new ng.Calendar({
+//         this.input: 'date1',
+//         this.start_date: 'last year',
+//         this.end_date: 'year + 5',
+//         this.display_date: new Date()
+//     });
+//  });
+      //  $(document).ready(function () {
+       //
+      //          $('#example1').datepicker({
+      //              format: "dd/mm/yyyy"
+      //          });
+       //
+      //      });
 
 
  Edit(mapmovieDate)
