8ecd1b97abe34cce61a70386abfb92c8b7ab1e7f
sathishkitkat <sathishkitkat001@gmail.com>
2017-05-29 20:01:36 +0530

    Add files via upload

--- movies/movies.controller.js
+++ movies/movies.controller.js
@@ -7,38 +7,65 @@ class MoviesComponent {
     this.message = 'Hello';
     this.$http = $http;
     this.moviesData = [];
-    this.socket =socket ;
+    this.socket =socket;
     this.data ='';
     this.movieObj= [];
-    // this.key ='';
+     this.key ='';
     $scope.$on('$destory',function() {
-      this.socket.unsyncupdates('thing');
+      this.socket.unsyncupdates('moviesendpoint');
     });
   }
   getmovie(){
-    this.$http.get('http://www.omdbapi.com/?t='+this.name+'&y='+this.year+'&plot=full&r=json').then (response =>{
-    console.log(response.data);
-    for(var key in response);
-    {
-     if(key=='Title'|| key=='Year' || key== 'Language' || key== 'Poster' || key== 'Genre' || key== 'Director' || key== 'Actors' || key== 'Plot')
-         {
-        this.movieObj[key] = response[key];
-      }
-     console.log(this.movieObj);
+  //   this.$http.get('http://www.omdbapi.com/?t='+this.name+'&y='+this.year+'&plot=full&r=json').then (response =>{
+  //   console.log(response.data);
+  //
+  //   for(var key in response.data)
+  //   {
+  //    if(key=='Title'|| key=='Year' || key== 'Language' || key== 'Poster' || key== 'Genre' || key== 'Director' || key== 'Actors' || key== 'Plot' || key== 'Response')
+  //        {
+  //       this.movieObj[key] = response.data[key];
+  //     }
+  //      this.socket.syncUpdates('moviesendpoint', this.movieObj);
+  //    console.log(this.movieObj);
+  //    }
+  //
+  // });
+
+  this.$http.get('https://api.themoviedb.org/3/search/movie?query='+this.name+'&api_key=585e2e0ebf8f161eea524634243e0f3a').then(response=>{
+
+    console.log(response.data.results);
+    for(var i=0;i<response.data.results.length;i++){
+    for(var key in response.data.results[i])
+      {
+       if(key=='response.data.results[i].title'|| key=='response.data.results[i].poster_path' || key== 'response.data.results[i].original_language' || key== 'response.data.results[i].overview')
+           {
+          this.movieObj[key] = response.data[key];
+        }
+         this.socket.syncUpdates('moviesendpoint', this.movieObj);
+       console.log(this.movieObj);
+       }
      }
-  });
+  })
+
+}
+  Addmovies() {
+this.$http.post('/api/moviesendpoints', {
+     Title: this.movieObj.Title,
+     Language: this.movieObj.Language,
+     Genre: this.movieObj.Genre,
+     Director: this.movieObj.Director,
+     Actors: this.movieObj.Actors,
+     Year: this.movieObj.Year,
+     Poster: this.movieObj.Poster,
+     Runtime: this.movieObj.Runtime
+    // Status: false
+   }).then (response=> {
+   console.log(response);
+  alert("Movie added successfully");
+
+ });
 }
 
-
- // AddMovies() {
-  //  this.$http.post('/api/moviesendpoints',{
-  //    name: this.name,
-  //    year: this.year
-  //  }).
-  //   then (response=> {
-  //   console.log(response);
-  //   });
-  // }
 
 $onInit() {
     this.$http.get('/api/moviesendpoints').then (response =>{
@@ -46,12 +73,19 @@ $onInit() {
     this.socket.syncUpdates('moviesendpoints', this.moviesData);
   });
  }
+
+Delete(movies)
+ {
+ this.$http.delete('/api/moviesendpoints/' + movies._id).then(response => {
+ console.log("delete");
+   });
+ }
 };
 angular.module('ticketbookingApp')
   .component('movies', {
     templateUrl: 'app/movies/movies.html',
     controller: MoviesComponent,
-    controllerAs: 'moviesCtrl'
+    controllerAs: '$moviesCtrl'
   });
 
 })();
