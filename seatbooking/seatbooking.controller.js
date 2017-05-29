ae06a453e67fe8497eabb2e3199197d856288430
sathishkitkat <sathishkitkat001@gmail.com>
2017-05-29 20:01:07 +0530

    Add files via upload

--- seatbooking/seatbooking.controller.js
+++ seatbooking/seatbooking.controller.js
@@ -3,42 +3,94 @@
 (function(){
 
 class SeatbookingComponent {
-  constructor($http, $scope, socket) {
+  constructor($http, $scope, socket,$cookies,$location) {
     this.message = 'Hello';
-    this.$http = $http;
-    this.seatbooking = [];
-    this.$socket = socket;
-    this.data = '';
-    $(document).ready(function() {
-      $('.seaton').click(function() {
-        var id = $(this).attr('id');
-       $('#' + id).css('background-color', 'green');
-      });
-    });
-  }
-  Addseatbooking(){
+   this.$http = $http;
+   this.$cookies=$cookies;
+   this.$scope=$scope;
+   this.$location=$location;
+   this.seatbooking = [];
+   this.$socket = socket;
+   this.scope =[];
+   this.data = [];
+   this.minfo=[];
+   this.bktitle='';
+   this.stime=[];
+   this.mapDates=[];
+   this.goCats = false;
+   this.sn=[];
+   this.bkdata=[];
+   this.s=[];
+  //  this.seat=false;
+    // this.seatArray = [];
+}
 
-    this.$http.post('/api/seatbookingendpoints',{
-       title:this.title,
-     city:this.city,
-       data:this.title,
-       amount:this.title,
-       showtime:this.showtime
-       numberofseat:this.title,
-       seatnumber:this.title,
-       cardno:this.title,
-      }).then (response=> {
-        console.log(response);
-          console.log("seatbooking added");
-    })
-  }
-  $onInit(){
-    this.$http.get('/api/seatbookingendpoints/').then(response => {
-      this.seatbookingdata = response.data;
-      this.socket.syncUpdates('seatbookingendpoints', this.seatbookingdata);
-    })
+bkeddata()
+{
+  for(var i=0;i<this.bkdata.length;i++)
+  {
+    if(this.bkdata[i].moviename==this.bktitle&&this.bkdata[i].cityname==this.city&&this.bkdata[i].tname==this.Theatre&&this.bkdata[i].date==this.date&&this.bkdata[i].time==this.showtime)
+    {
+    this.s.push(this.bkdata[i].seatno);
+    console.log(this.s);
+    for(var a=0;a<this.s.length;a++){
+for(var j=0;j<this.s[a].length;j++)
+{
+        var id=this.s[a][j];
+        console.log(id);
+        $('#'+id).attr('src','assets/Images/chair-2-512 (3).gif');
+      }
+    }
+
+    }
   }
 }
+  $onInit(){
+    var info = this.$cookies.getObject('info');
+    console.log(info);
+    if(info!=undefined){
+    var minfo=info.movieinfo.bk;
+     console.log(minfo);
+     this.bktitle=info.movieinfo.tit;
+     this.minfo=minfo;
+     console.log(this.bktitle);
+     console.log(this.minfo);
+     document.getElementById("mname").innerHTML=this.bktitle;
+     document.getElementById("mtit").innerHTML=this.bktitle;
+}
+this.$http.get('/api/paymentendpoints').then(response => {
+this.bkdata = response.data;
+console.log(this.bkdata);
+
+});
+ }
+pay()
+{
+  this.sn=seats;
+  console.log(seats);
+  console.log(this.sn);
+  this.$http.post('/api/seatbookingendpoints', {
+    title:  document.getElementById("mtit").innerHTML,
+Theatre:this.Theatre,
+city: this.city,
+date: this.date,
+showtime: this.showtime,
+amount: document.getElementById("subtotal").innerHTML,
+seatnumber: this.sn
+     }).then(response=> {
+     console.log(response);
+     var obj = {
+       bkinfo:response.data};
+         console.log(obj);
+                         this.$cookies.putObject('bkinfo', obj);
+
+                       this.$location.path('/payment');
+
+
+   });
+}
+};
+
 
 angular.module('ticketbookingApp')
   .component('seatbooking', {
